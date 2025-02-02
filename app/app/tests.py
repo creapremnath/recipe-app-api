0fd72891b64c3a9e292913f98233357b36f9ef21
“creapremnath” <creativepremnath@gmail.com>
2025-02-02 13:10:54 +0530

    Postgresql db added #Fixed linting issue 2

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,12 +1,10 @@
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class TestCalculator(SimpleTestCase):
     def test_add(self):
-        self.assertEqual(calc.add(2 , 3) , 5)
-        
+        self.assertEqual(calc.add(2, 3), 5)  # Fixed spacing before commas
+
     def test_subtract(self):
-        self.assertEqual(calc.subtract(5 , 3) , 2)
-        
-        
+        self.assertEqual(calc.subtract(5, 3), 2)  # Fixed spacing before commas
