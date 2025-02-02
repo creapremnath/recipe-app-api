3c5e690ef567b517fc84fb1554408c71041b1145
“creapremnath” <creativepremnath@gmail.com>
2025-02-02 12:51:44 +0530

    Postgresql db added

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ docker-compose up
 #For any error
 
 docker-compose down
+
+
+
+#to run the tests
+# --rm removes the container after execution
+docker-compose run  --rm app sh -c "python manage.py test" 
---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class TestCalculator(SimpleTestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(2, 3), 5)
+        
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(5, 3), 2)
