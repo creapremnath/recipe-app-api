1a5ef1e7459cd1151dd610f55e51f368d2f6a950
“creapremnath” <creativepremnath@gmail.com>
2025-02-16 22:07:35 +0530

    #89 build Recipe API Completed

--- app/core/models.py
+++ app/core/models.py
@@ -46,6 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+
 class Recipe(models.Model):
     """Recipe in the System"""
     user = models.ForeignKey(
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,11 +13,23 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Viewset for managing recipe APIs"""
-    serializers_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer  # Corrected
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+
     def get_queryset(self):
         """Return only the recipes for the authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
