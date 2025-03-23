e69dd0abcff40d3c0902a7c6df607bea87f97204
“creapremnath” <creativepremnath@gmail.com>
2025-03-23 21:54:35 +0530

    changes addded for env and settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media/'
-STATIC_URL = '/vol/web/static/'
+STATIC_ROOT = '/vol/web/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
